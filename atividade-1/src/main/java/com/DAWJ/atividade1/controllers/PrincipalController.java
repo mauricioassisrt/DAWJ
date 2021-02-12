bf34e277d15dbd59223ff08dcd6726d32635fbe9
Mauricio de Assis <mauriciox8mm@hotmail.com>
2021-02-12 17:06:34 -0300

    exercicio 1 concluído

---
+++ atividade-1/src/main/java/com/DAWJ/atividade1/controllers/PrincipalController.java
@@ -0,0 +1,24 @@
+package com.DAWJ.atividade1.controllers;
+
+import javax.swing.JOptionPane;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PrincipalController {
+    public Integer contador =1;
+    
+    @GetMapping("contador")
+    public ModelAndView recebeParam(Model model) {
+        ModelAndView mv = new ModelAndView("index");
+        contador++;
+        model.addAttribute("contador" , contador);
+        mv.addObject(model);
+        return mv;
+    }
+  
+}
